ff9149ac8148cdcec961f50eefd94fa6c24c48bd
nurirfan885-sketch <nurirfan885@gmail.com>
2026-01-28 14:06:24 +0700

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,14 +1,12 @@
 const { createClient } = require('@supabase/supabase-js');
 const { Buffer } = require('buffer');
 const supabase = createClient('https://lmdylqnkidbbreqqfxqm.supabase.co', 'sb_secret_Wnma0adzxT-_bxecEpebjw_HuLcHVdj');
-
 module.exports = async (req, res) => {
   try {
-    const { fileName, fileType, bucket } = req.query; // Kita tambah parameter bucket
-    const targetBucket = bucket || 'arsip_files';
+    const { fileName, fileType, bucket } = req.query;
     const buffer = Buffer.from(req.body, 'base64');
-    await supabase.storage.from(targetBucket).upload(fileName, buffer, { contentType: fileType, upsert: true });
-    const { data: { publicUrl } } = supabase.storage.from(targetBucket).getPublicUrl(fileName);
+    await supabase.storage.from(bucket).upload(fileName, buffer, { contentType: fileType, upsert: true });
+    const { data: { publicUrl } } = supabase.storage.from(bucket).getPublicUrl(fileName);
     return res.status(200).json({ publicUrl });
   } catch (e) { return res.status(500).json({ error: e.message }); }
 };
